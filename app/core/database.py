1307f6e2926b3bc176f71a690dcbd7ff17606e1e
wil-wu <wil-wu@outlook.com>
2026-03-17 10:33:08 +0800

    Add postgres support

--- app/core/database.py
+++ app/core/database.py
@@ -10,7 +10,7 @@ from app.core.enum import JobStatus, JobType
 
 async_engine = create_async_engine(
     settings.database_url,
-    connect_args={"check_same_thread": False},
+    connect_args={"check_same_thread": False} if settings.database_url.startswith("sqlite") else {},
 )
 
 async_session = async_sessionmaker(
